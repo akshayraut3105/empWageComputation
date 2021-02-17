57cca1afef39c353d7c02d4a5fef39557a5c29e7
Akshay <akshayraut3105@gmail.com>
2021-02-17 21:52:33 +0530

    added empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -12,3 +12,25 @@ wage_per_Hr=20
 no_of_Hr=8
 echo "the daily employee wage is $((attendance * wage_per_hr * no_per_Hr ))
 
+	#attendance check for full time and part time
+
+
+attendence=$(( RANDOM % 3 ))
+case $attendence in 
+    0)
+        echo "Employee is absent"
+        no_of_hrs_per_day=0
+        ;;
+    1)
+        echo "Employee is full time present"
+        no_of_hrs_per_day=8
+        ;;
+    2)
+        echo "Employee is part time present"
+        no_of_hrs_per_day=4
+        ;;
+esac
+
+wage_per_hr=20
+wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
+echo "The daily wage of the employee is $wage_per_day "
